6f4109e0ae1edd8b4d57e0af307cbb1aacb00874
Alexander <sheepsaurus@gmail.coim>
2022-06-13 00:41:21 +0200

    Product View

---
+++ components/Products.jsx
@@ -0,0 +1,35 @@
+export default function Products({ products }) {
+
+    return (
+        <div className="bg-white">
+            <div className="max-w-2xl mx-auto py-4 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+  
+                <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                    {products.map((product) => (
+                        <div key={product.id} className="group relative hover:bg-green-200 cursor-pointer">
+                            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden  group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                                <img
+                                    src={product.imageSrc}
+                                    alt={product.imageAlt}
+                                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                                />
+                            </div>
+                            <div className="mt-4 flex justify-between">
+                                <div>
+                                    <h3 className="text-sm text-gray-700">
+                                        <a href={product.href}>
+                                            <span aria-hidden="true" className="absolute inset-0" />
+                                            {product.title}
+                                        </a>
+                                    </h3>
+                                    <p className="mt-1 text-sm text-gray-500">{product.description}</p>
+                                </div>
+                                <p className="text-sm font-medium text-gray-900">kr. {product.price},-</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
