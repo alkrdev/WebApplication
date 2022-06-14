9adfd1862afb8185d5e55ea985e2227cc35cbf3e
Alexander <sheepsaurus@gmail.coim>
2022-06-14 07:29:08 +0200

    Moved out of _app

--- pages/_app.js
+++ pages/_app.js
@@ -11,31 +11,4 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-MyApp.getInitialProps = ({req, res}) => {
-  const cookies = new Cookies(req, res)
-
-  var id = cookies.get('cartid')
-  
-  if (id) return;
-  const newUuid = uuidv4();
-
-  console.log("BEFORE FETCHING")
-  fetch(process.env.NEXT_PUBLIC_WEB_SERVER + "/carts/", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify({
-      id: newUuid
-    })
-  }).then((res) => {
-    console.log("COOKIE SET ATTEMPT")
-    if (res.ok) {
-      cookies.set('cartid', newUuid, {
-        httpOnly: false
-      }) 
-    }
-  })
-}
-
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import { v4 as uuidv4 } from 'uuid';
 import Nav from "./../components/Nav"
 import FilterList from "./../components/FilterList"
 
-export default function Home() {
+const Home = () => {
   const [cartItems, setCartItems] = useState([])
 
   useEffect(() => {
@@ -60,3 +60,32 @@ export default function Home() {
     </div>
   )
 }
+
+Home.getInitialProps = ({req, res}) => {
+  const cookies = new Cookies(req, res)
+
+  var id = cookies.get('cartid')
+  
+  if (id) return;
+  const newUuid = uuidv4();
+
+  console.log("BEFORE FETCHING")
+  fetch(process.env.NEXT_PUBLIC_WEB_SERVER + "/carts/", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      id: newUuid
+    })
+  }).then((res) => {
+    console.log("COOKIE SET ATTEMPT")
+    if (res.ok) {
+      cookies.set('cartid', newUuid, {
+        httpOnly: false
+      }) 
+    }
+  })
+}
+
+export default Home
